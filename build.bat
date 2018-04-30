b839d7f17f96d15c3ba7566bea70efac6008337d
mitaken <mitaken@outlook.com>
2018-04-30 22:59:13 +0900

    update build command
    build only AnyCPU

--- build.bat
+++ build.bat
@@ -1,9 +1,8 @@
 @echo off
 
 RD /S /Q %~dp0\OpenCsvByExcel\bin
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" %~dp0\OpenCsvByExcel.sln /p:Configuration=Release /p:Platform=x86 /p:CodeAnalysisRuleSet= /t:Clean;Build
-"%ProgramFiles(x86)%\MSBuild\14.0\Bin\msbuild.exe" %~dp0\OpenCsvByExcel.sln /p:Configuration=Release /p:Platform=x64 /p:CodeAnalysisRuleSet= /t:Clean;Build
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" %~dp0\OpenCsvByExcel.sln /p:Configuration=Release /p:Platform="Any CPU" /p:CodeAnalysisRuleSet= /t:Clean;Build
 
-powershell -ExecutionPolicy RemoteSigned -Command %~dp0\binary.ps1
+powershell -ExecutionPolicy RemoteSigned -Command %~dp0\build.ps1
 
 pause
